cfaf6efff14a561e646372fbba2766b644024ecf
MatheusAraujo42 <matheus.araujo@corebiz.com.br>
2018-12-10 15:16:03 -0200

    90. Including Cookies with GraphQL Requests

--- client/index.js
+++ client/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 
+
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
@@ -14,7 +23,7 @@ const Root = () => {
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
-          
+
         </Route>
       </Router>
     </ApolloProvider>
