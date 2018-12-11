95dc16d24ece20aeb5d3f6f73dc0b3c244f99285
MatheusR42 <matheus.araujo.r42@gmail.com>
2018-12-11 09:05:33 -0200

    99. Refreshing the Current User

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+import query from '../queries/current-user';
+import mutation from '../mutations/login';
+import { graphql } from 'react-apollo';
 
 class LoginForm extends Component {
+    onSubmit({ email, password }) {
+        this.props.mutate({
+            variables: {
+                email,
+                password
+            },
+            refetchQueries: [{ query }]
+        })
+    }
+
     render() {
         return (
-            <div>LoginForm</div>
+            <div>
+                <AuthForm 
+                    onSubmit={this.onSubmit.bind(this)}
+                />
+            </div>
         )
     }
 }
 
-export default LoginForm;
+export default graphql(mutation)(LoginForm);
